83f042c5120e0fd61349bb11313a0e4001a035d3
pradeep-ravan <triplehthegame143@gmail.com>
2021-05-23 19:53:52 +0530

    ...

---
+++ 8_camera/activity/script.js
@@ -0,0 +1,37 @@
+let constraints = {video:true, audio:true};
+let videoPlayer = document.querySelector('video');
+let videoPlayer = document.querySelector('#record-video');
+let mediaRecorder;
+let recordState = false;
+let chunks = [];
+        vidRecordBtn.addEventListener("click",function(){
+            if(mediaRecorder!=undefined){
+                if(recordState==false){
+                    recordState=true;
+                    mediaRecorder.start();
+                    vidRecordBtn.innerText = 'Recording....'
+                }
+                else{
+                    recordState=false;
+                    mediaRecorder.stop();
+                    vidRecordBtn.innerText = 'Record';
+                }
+            }
+        })
+        navigator.mediaDevices.getUserMedia(constraints).then(function(mediaStream){
+            videoPlayer.srcObject=mediaStream;
+            mediaRecorder = new MediaRecorder(mediaStream);
+            mediaRecorder.ondataavailable = function(e){
+                chunks.push(e.data)
+            }
+            mediaRecorder.onstop=function(){
+                let blob = new Blob(chunks,{type:'video/mp4'});
+                chunks =[];
+                let blobUrl = URL.createObjectURL(blob);
+                link.href = blobUrl;
+                link.click();
+                link.remove();
+            }
+        }).catch(function(err){
+            console.log(err);
+        })
