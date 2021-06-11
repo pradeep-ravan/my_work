6880f4b1231d0e364ac6780880afc1dc2659aa8a
pradeep-ravan <triplehthegame143@gmail.com>
2021-06-11 16:55:34 +0530

    ......

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -1,76 +1,92 @@
 import React, { Component } from 'react'
+
 export default class Todo extends Component {
-    constructor(props){
-        super(props);
+    constructor()
+    {
+        super();
         this.state={
-            tasks: [{id:1,txt:'First task'},{id:2,txt:'Second task'},{id:3,txt:'Third task'}],
-            currTask:''
+            tasks:[{id:1,txt:'First task'},{id:2,txt:'Second Task'},
+            {id:3,txt:'Third Task'}],
+           
         }
+       
     }
-    handleChange=(e)=>{
-        let cval= e.target.value;
-        this.setState({currTask:cval});
-    }
-    handleClick=()=>{
-        //console.log(this.state.currTask);
-        //this.state.tasks.push({id:this.state.tasks.length+1,txt:this.state.currTask});
-        //this.state.currTask='';
-        let nta = [...this.state.tasks,{id:this.state.tasks.length+1,txt:this.state.currTask}];
+  
+    handleClick=(task)=>{
+       
+        let nta = [...this.state.tasks,
+            {id:this.state.tasks.length+1,txt:task}];
         this.setState({
-            tasks:nta,
-            currTask:''
+            tasks:nta
         })
+       
     }
-    onDelete=(id)=>{
+    onDelete =(id)=>{
         let nta = this.state.tasks.filter(obj=>{
             return obj.id!=id
         })
         this.setState({
             tasks:nta
         })
+
     }
     render() {
-        console.log('Rendered');
+
         return (
             <>
-            <InputComponent currTask={this.state.currTask}handleChange={this.handleChange} handleClick={this.handleClick} />
-            <TaskList tasks={this.state.tasks}onDelete={this.onDelete} />
+            <InputComponent  handleClick={this.handleClick} />
+            <TaskList tasks={this.state.tasks} 
+            onDelete={this.onDelete} />
             </>
         )
     }
 }
+
 class InputComponent extends Component {
-    constructor(props){
+    constructor(props)
+    {
         super(props);
+        this.state={
+            currTask:''
+        }
+    }
+    handleChange=(e)=>{
+        this.setState({currTask:e.target.value})
     }
     render() {
         return (
-            <div className='Input-container'>
-            <input type='text' value={this.props.currTask}onChange={this.props.handleChange} ></input>
-            <button onClick={this.props.handleClick} >Add</button>  
-            </div>
+            <div className='input-container'>
+            <input  type='text' value={this.state.currTask}
+             onChange={this.handleChange} ></input>
+            <button onClick={()=>{
+                this.props.handleClick(this.state.currTask);
+                this.setState({currTask:''})
+            }} >
+                Add</button>
+       </div>
         )
     }
 }
 class TaskList extends Component {
-    constructor(props){
+    constructor(props)
+    {
         super(props);
     }
     render() {
         return (
             <div className='class-list'>
                 <ul>
-                    {
+                    { 
                         this.props.tasks.map(task=>(
                             <li key={task.id}>
                                 <h1>{task.txt}</h1>
-                                <button onClick={()=>this.props.onDelete(task.id)} >Delete</button>
+                                <button 
+  onClick={()=>this.props.onDelete(task.id)} >Delete</button>
                             </li>
-                        ))
+        ))
                     }
-                </ul>
-            </div>
+                </ul>    
+            </div>      
         )
     }
 }
-
