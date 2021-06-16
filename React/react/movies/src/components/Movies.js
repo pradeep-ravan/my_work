52883753305514970c457aa529b7f2090b79505e
pradeep-ravan <triplehthegame143@gmail.com>
2021-06-17 02:52:08 +0530

    .....

---
+++ React/react/movies/src/components/Movies.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import { getMovies } from './getMovies'
+export default class Movies extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            movies:getMovies(),
+            currSearchText:'',
+            filterMovies:getMovies()
+        }
+    }
+    onDelete = (id)=>{
+        let filterMovies = this.state.movies.filter(movieObj=>{
+            return movieObj._id!=id
+        })
+        this.setState({
+            movies:filterMovies,
+        })
+    }
+
+    handleChange=(e)=>{
+        this.setState({currSearchText:e.target.value})
+    }
+
+    render() {
+        let {movies} = this.state;
+        return (
+            <div className='row'>
+                <div className='col-3'>
+                    <h1>hello</h1>
+                </div>
+                <div className='col-9'>
+                    <input value={this.currSearchText} onChange={this.handleChange} type='txt'></input>
+                    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Title</th>
+      <th scope="col">Genre</th>
+      <th scope="col">Stock</th>
+      <th scope="col">Rate</th>
+    </tr>
+  </thead>
+  <tbody>
+  {
+    movies.map(movieObj=>(
+    <tr scope='row' key={movieObj._id} >  
+        <td></td> 
+        <td>{movieObj.title}</td>   
+        <td>{movieObj.genre.name}</td>
+        <td>{movieObj.numberInStock}</td>
+        <td>{movieObj.dailyRentalRate}</td>
+        <td><button type='button' class="btn btn-danger" onClick={()=>this.onDelete(movieObj._id)}>Delete</button></td>                          
+    </tr>
+    ))
+  }
+  </tbody>
+</table>
+                </div>   
+            </div>
+        )
+    }
+}
+
+
