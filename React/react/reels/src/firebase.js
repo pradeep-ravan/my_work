66bb6f8c51d2cd40c58148044c735d747303e89e
pradeep-ravan <triplehthegame143@gmail.com>
2021-07-08 21:41:59 +0530

    .......

---
+++ React/react/reels/src/firebase.js
@@ -0,0 +1,22 @@
+import firebase from "firebase/app"
+import "firebase/auth"
+import "firebase/storage"
+import "firebase/firestore"
+firebase.initializeApp(
+    {
+        apiKey: "AIzaSyD2XilxxQvinKFKViHf6xSduNqda3wM69I",
+        authDomain: "reels-class-32d69.firebaseapp.com",
+        projectId: "reels-class-32d69",
+        storageBucket: "reels-class-32d69.appspot.com",
+        messagingSenderId: "531669142719",
+        appId: "1:531669142719:web:422301cecbf9e93a810a0a"
+      }
+)
+export const auth = firebase.auth();
+const firestore = firebase.firestore();
+export const database ={
+    users:firestore.collection('users'),
+    getCurrentTimeStamp : firebase.firestore.FieldValue.serverTimestamp
+}
+export const storage = firebase.storage();
+// export default firebase;
