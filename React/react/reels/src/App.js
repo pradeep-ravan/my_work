e9e11e4a528385cdbb0ccc5e9ea472d76e7de2d2
pradeep-ravan <triplehthegame143@gmail.com>
2021-07-20 01:28:13 +0530

    ......

--- React/react/reels/src/App.js
+++ React/react/reels/src/App.js
@@ -3,17 +3,22 @@ import './App.css';
 import Signup from './Components/Signup'
 import AuthProvider from './Context/AuthProvider';
 import Main from './MaterialUI/Mains';
-//import Login from './Components/Login';
+import Login from './Components/Login';
 import Ioa from './Components/Ioa';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import PrivateRoute from './Components/PrivateRoute';
+import Feed from './Components/Feed';
 function App() {
   return (
-    // <AuthProvider>
-    // <Signup/>
-   // <Login/>
-    // </AuthProvider>
-   // <Main/>
-   
-    <Ioa/>
+    <Router>
+      <AuthProvider>
+      <Switch>
+        <PrivateRoute exact path='/' component={Feed}/>
+        <Route path='/login' component={Login}/>
+        <Route path='/signup' component={Signup}/>
+      </Switch>
+      </AuthProvider>
+    </Router>
   );
 }
 
