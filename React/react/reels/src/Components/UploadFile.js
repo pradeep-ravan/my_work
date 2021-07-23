7e2bf67457ecae23a823523167bdf58bb5ff2602
pradeep-ravan <triplehthegame143@gmail.com>
2021-07-23 14:47:43 +0530

    .......

--- React/react/reels/src/Components/UploadFile.js
+++ React/react/reels/src/Components/UploadFile.js
@@ -6,98 +6,106 @@ import {v4 as uuidv4} from 'uuid';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import {storage,database} from '../firebase'
 const useStyles = makeStyles((theme) => ({
-        
+    
   }));
   
 function UploadFile(props) {
     const classes = useStyles();
-    const [loading,setLoading] = useState(false);
-    const [error,setError] = useState(null);
-    const types=['video/mp4','video/webm','video/ogg'];
+    const[loading,setLoading] = useState(false);
+    const[error,setError] = useState(null);
+    const types =['video/mp4','video/webm','video/ogg'];
     const onChange=(e)=>{
-        const file = e?.target.file[0];
+        const file = e?.target?.files[0];
         console.log(file);
         if(!file){
-          setError('Please select a file');
-          setTimeout(()=>{setError(null)},2000);
-          return;
+            setError('Please select a file');
+            setTimeout(()=>{setError(null)},2000)
+            return;
         }
-        if(types.indexOf(file.type)==-1){
-          setError('Please select a file');
-          setTimeout(()=>{setError(null)},2000);
-          return;
+
+        if(types.indexOf(file.type)==-1)
+        {
+            setError('Please select a video file');
+            setTimeout(()=>{setError(null)},2000)
+            return;
         }
-        if(file.size/(1024*1024)>100){
-          setError('selected file is too big');
-          setTimeout(()=>{setError(null)},2000);
-          return;
+
+        if(file.size/(1024*1024)>100)
+        {
+            setError('The selected file is too big');
+            setTimeout(()=>{setError(null)},2000)
+            return;
         }
         const id = uuidv4();
         const uploadTask = storage.ref(`/posts/${props.userData.userId}/${file.name}`).put(file);
         uploadTask.on('state_changed',fn1,fn2,fn3);
         function fn1(snapshot){
-          var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log('Upload is' + progress + '% done');
-      }
+            var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log('Upload is ' + progress + '% done');         
+        }
         function fn2(error){
-          setError(error);
-          setTimeout(()=>{
-              setError(null)
-          },2000);
-          setLoading(false)
-      }
-      async function fn3(){
+            setError(error);
+            setTimeout(()=>{
+                setError(null)
+            },2000);
+            setLoading(false)
+        }
+        async function fn3(){
           setLoading(true);
           uploadTask.snapshot.ref.getDownloadURL().then(url=>{
-            let obj={
-              comments:[],
-              likes:[],
-              pId:id,
-              pUrl:url,
-              uName:props?.userData?.username,
-              uProfile:props?.userData?.profileUrl,
-              userId:props?.userData?.userId,
-              createAt:database.getCurrentTimeStamp(),
-            }
-            console.log(obj);
-            console.log(props.userData);
-            database.posts.add(obj).then(async docRef=>{
-              console.log(docRef);
-              let res = await database.users.doc(props.userData.userId).update({
-                postIds:[...props.userData.postIds,docRef.id]
+              let obj  ={
+                  comments:[],
+                  likes:[],
+                  pId:id,
+                  pUrl:url,
+                  uName:props?.userData?.username,
+                  uProfile:props?.userData?.profileUrl,
+                  userId:props?.userData?.userId,
+                  createdAt:database.getCurrentTimeStamp()
+              }
+              console.log(obj);
+              console.log(props.userData);
+              database.posts.add(obj).then(async docRef=>{
+                  console.log(docRef);
+                  let res = await database.users.doc(props.userData.userId).update({
+                      postIds:[...props.userData.postIds,docRef.id]
+                  })
+              }).then(()=>{
+                  setLoading(false)
+              }).catch(e=>{
+                  setError(e);
+                  setTimeout(()=>{
+                      setError(null)
+                  },2000);
+                  setLoading(false)
               })
-            }).then(()=>{
-              setLoading(false)
-            }).catch(e=>{
-              setError(e);
-              setTimeout(()=>{
-                setError(null);
-              },2000);
-              setLoading(false);
-            })
-          }) 
-      }
+          })
+        }
     }
+
     return (
-      <>
-      {
-          error!=null? <Alert severity="error">{error}</Alert>:<>
-          <input
-          color='primary'
-          type='file'
-          onChange={onChange}
-          id='icon-button-file'
-          style={{display:'none'}}
-          />
-          <label htmlFor='icon-button-file'>
-          <Button disabled={loading} variant="outlined" component='span' className={classes.button} size='medium' color="secondary">
-            Secondary
-          </Button>
-          </label>
-          {loading?<LinearProgress color='secondary' style={{marginTop:'6%'}}/>:<></>}
-          </>
-      }
-      </>
+        <>
+        {
+            error!=null? <Alert severity="error">{error}</Alert>:<>
+            <input 
+            color='primary'
+            type='file'
+            onChange={onChange}
+            id='icon-button-file'
+            style={{display:'none'}}
+            />
+            <label htmlFor='icon-button-file'>
+            <Button disabled={loading} variant="outlined" component='span' className={classes.button} 
+            size='medium' color="secondary">
+                Secondary
+            </Button>
+
+            </label>
+            {loading?<LinearProgress color='secondary' style={{marginTop:'6%'}} />:<></>}
+            </>
+
+        }
+        </>
     )
 }
 
