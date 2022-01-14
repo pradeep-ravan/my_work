84b2dac83f1535ece93c01afdd0d2d91c428eb1f
pradeep-ravan <triplehthegame143@gmail.com>
2022-01-14 21:49:54 +0530

    ....

--- Backend/FoodApp/app.js
+++ Backend/FoodApp/app.js
@@ -14,45 +14,61 @@ app.use(express.json());
 //(ii post : to submit data to backend server
 //(iii)patch : to update something
 //(iv)delete ; to delete something from backend server
-app.get("/",function (req, res){
-    console.log("hello from homepage");
-    res.send("<h1>Hello from Backend</h1>");
-})
+
 let user = {   
 };
 //getting data from server
 //giving data from server
 //crud app
 //create
-app.post("/user", function (req,res) {
+function createUser(req, res) {
     console.log("req.data", req.body);
     user = req.body;
     res.status(200).send("data received and user added");
-})
-//get
-app.get("/user",function (req, res){
+}
+function getUser(req, res){
     console.log("user request");
     // for sending key value pair
     res.json(user);
-})
-//update
-app.patch("/user", function (req, res) {
+}
+function updateUser(req, res) {
     let obj = req.body;
     for(let key in obj){
         user[key] = obj[key];
     }
     res.status(200).json(user);
-})
-//delete
-app.delete("/user", function (req, res) {
+}
+function deleteUser(req, res) {
     user = {};
     res.status(200).json(user);
-})
-//template routes
-app.get("/user/:id", function (req, res) {
+}
+function getUserById(req, res) {
+
     console.log(req.params.id);
     res.status(200).send("hello");
-})
+}
+//mounting in express
+const router = express.Router();
+app.use('/api/user',router);
+
+router.
+route("/")
+.get(getUser)
+.post(createUser)
+.patch(updateUser)
+.delete(deleteUser);
+router
+.route("/:id")
+.get(getUserById);
+// app.post("/api/user", createUser) ;
+// //get
+// app.get("/api/user", getUser);
+// //update
+// app.patch("/api/user", updateUser);
+// //delete
+// app.delete("/api/user", deleteUser);
+// //template routes
+// app.get("/api/user/:id", getUserById);
 //localhost:8080 ?
 app.listen(8080,function(){
     console.log("server started");
