ed279061bd5b894135833370ba65392d6f1d4a19
pradeep-ravan <triplehthegame143@gmail.com>
2022-01-17 17:34:06 +0530

    ...

---
+++ Backend/FoodApp/models/userModel.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+let {DB_LINK} = require("../../../secrets");
+mongoose.connect(DB_LINK).then(function (db) {
+    console.log(db);
+}).catch(function (err) {
+    console.log("err",err);
+})
