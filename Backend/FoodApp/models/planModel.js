4569871664b62c6ff2f2bb61c160a25fe5cf8a09
pradeep-ravan <triplehthegame143@gmail.com>
2022-02-27 17:02:12 +0530

    .....

---
+++ Backend/FoodApp/models/planModel.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const emailValidator = require("email-validator");
+let {DB_LINK} = require("../../../secrets");
+mongoose.connect(DB_LINK,{useNewUrlParser: true,
+        
+        useUnifiedTopology: true}).then(function () {
+    //console.log(db);
+    console.log("connected to db");
+}).catch(function (err) {
+    console.log("err",err);
+})
+
+
+
+
+//syntax
+const planSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true,"kindly pass the name"],
+        unique: true,
+        maxlength: [40, "Your plan length is more than 40 characters"],
+    },
+    duration: { 
+        type: Number,
+        required: [true, "you need to provide duration"] 
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    ratingsAverage: {
+        type: Number
+    },
+    discount: {
+        type: Number,
+        validate: {
+            validator: function () {
+                return this.discount < this.price;
+            },
+            message: "Discount must be less than actual price",
+        },
+    },
+})
+ 
+const planModel = mongoose.model("planModel", planSchema);
+
+
+ module.exports = planModel
